f246e10354fc245e05f9a75989535064228306ea
aaravsaki <asaki1@icloud.com>
2024-09-16 17:19:05 -0700

    fixed king movement

--- src/king.cpp
+++ src/king.cpp
@@ -5,40 +5,33 @@ King::King(PieceType piece_type, Team team, Coord position)
 {
 }
 
-
 std::vector<Coord> King::getMoves(const std::vector<std::vector<std::unique_ptr<Piece>>>& field) const
 {
     std::vector<Coord> moves;
-    int dy = 1;
+    std::vector<Coord> directions{
+        Coord{1,1}, 
+        Coord{1,0}, 
+        Coord{1,-1}, 
+        Coord{0,1}, 
+        Coord{0,-1}, 
+        Coord{-1,1}, 
+        Coord{-1,0}, 
+        Coord{-1,-1}
+    };
     Coord position = getCoord();
 
-    if (getTeam() == Team::white)
+    for (auto &[dr, dc] : directions)
     {
-        dy = -1;
-    }
-    
-    // Sliding moves (1 or 2 spaces forward)
-    if (!field[position.row][position.col + dy])
-    {
-        moves.push_back(Coord{position.row, position.col + dy});
-        if (position.row == 6 && !field[position.row][position.col  + 2 * dy])
+        Coord target{position.row + dr, position.col + dc};
+        if (target.row >= 0 && target.col >= 0 && target.row < field.size() && target.col < field.size())
         {
-            moves.push_back(Coord{position.row, position.col + 2 * dy});
+            if (!field[target.row][target.col] || field[target.row][target.col]->getTeam() != getTeam())
+            {
+                moves.push_back(target);
+            }
         }
     }
 
-    // Attacking moves (one space diagonally)
-    if (position.row < 7 && field[position.row + 1][position.col + dy] && 
-        field[position.row + 1][position.col - 1]->getTeam() != getTeam())
-    {
-        moves.push_back(Coord{position.row + 1, position.col + dy});
-    }
-
-    if (position.row > 0 && field[position.row - 1][position.col + dy] && 
-        field[position.row - 1][position.col - 1]->getTeam() != getTeam())
-    {
-        moves.push_back(Coord{position.row + 1, position.col + dy});
-    }
 
     return moves;
 }
